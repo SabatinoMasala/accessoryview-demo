2978cae45c1e3a46098583b6bb56c1ee55ef126f
Sabatino Masala <Masalasabatino@gmail.com>
2015-06-26 13:13:48 +0200

    added textview to view hierarchy

--- AccessoryDemo/DetailViewController.swift
+++ AccessoryDemo/DetailViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 class DetailViewController: UIViewController {
   
   weak var accessoryView: UITextView?;
+  weak var dummyAccessoryView: UITextView?;
   
   override var inputAccessoryView: UIView? {
     get {
@@ -25,8 +26,73 @@ class DetailViewController: UIViewController {
     }
   }
   
+  override func viewDidLoad() {
+    
+    self.inputAccessoryView?.hidden = true;
+    
+    let dummyAccessoryView = UITextView(frame: CGRectZero);
+    self.view.addSubview( dummyAccessoryView );
+    self.dummyAccessoryView = dummyAccessoryView;
+    self.setupConstraintsForDummyAccessoryView();
+    self.dummyAccessoryView?.text = "This is some text";
+    
+  }
+  
   override func viewDidAppear(animated: Bool) {
     super.viewDidAppear(animated);
+    
+    self.dummyAccessoryView?.removeFromSuperview();
+    self.inputAccessoryView?.hidden = false;
+    
+  }
+  
+  func setupConstraintsForDummyAccessoryView() {
+    
+    if let dummyAccessoryView = self.dummyAccessoryView {
+      
+      let widthConstraint: NSLayoutConstraint = NSLayoutConstraint(
+        item: dummyAccessoryView,
+        attribute: NSLayoutAttribute.Width,
+        relatedBy: NSLayoutRelation.Equal,
+        toItem: self.view,
+        attribute: NSLayoutAttribute.Width,
+        multiplier: 1.0,
+        constant: 0
+      );
+      let heightConstraint: NSLayoutConstraint = NSLayoutConstraint(
+        item: dummyAccessoryView,
+        attribute: NSLayoutAttribute.Height,
+        relatedBy: NSLayoutRelation.Equal,
+        toItem: self.view,
+        attribute: NSLayoutAttribute.Height,
+        multiplier: 0,
+        constant: 50
+      );
+      let bottomConstraint: NSLayoutConstraint = NSLayoutConstraint(
+        item: dummyAccessoryView,
+        attribute: NSLayoutAttribute.Bottom,
+        relatedBy: NSLayoutRelation.Equal,
+        toItem: self.view,
+        attribute: NSLayoutAttribute.Bottom,
+        multiplier: 1.0,
+        constant: 0
+      );
+      let leftConstraint: NSLayoutConstraint = NSLayoutConstraint(
+        item: dummyAccessoryView,
+        attribute: NSLayoutAttribute.Left,
+        relatedBy: NSLayoutRelation.Equal,
+        toItem: self.view,
+        attribute: NSLayoutAttribute.Left,
+        multiplier: 1.0,
+        constant: 0
+      );
+      
+      dummyAccessoryView.translatesAutoresizingMaskIntoConstraints = false;
+      
+      self.view.addConstraints([widthConstraint, heightConstraint, leftConstraint, bottomConstraint]);
+      
+    }
+    
   }
   
   required init(coder aDecoder: NSCoder) {
